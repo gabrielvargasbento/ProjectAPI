044ad853f1b626fa0383e0ed38e6debd82581b10
Gabriel Vargas <gabriel.vargas@dtidigital.com.br>
2024-01-15 15:47:26 -0300

    start model tests

---
+++ ProjectAPITests/ModelTests/HarryPotterTests.swift
@@ -0,0 +1,13 @@
+//
+//  HarryPotterTests.swift
+//  ProjectAPITests
+//
+//  Created by Gabriel Vargas on 15/01/24.
+//
+
+import Quick
+import Nimble
+
+final class HarryPotterTests: QuickSpec {
+
+}
---
+++ ProjectAPITests/ModelTests/RepositoryTests.swift
@@ -0,0 +1,13 @@
+//
+//  RepositoryTests.swift
+//  ProjectAPITests
+//
+//  Created by Gabriel Vargas on 15/01/24.
+//
+
+import Quick
+import Nimble
+
+final class RepositoryTests: QuickSpec {
+
+}
